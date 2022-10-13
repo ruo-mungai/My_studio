c8b566be2c84ad6e8559d9af7e13a83d03932829
Patrick <ruomungai@gmail.com>
2022-10-13 17:03:50 +0300

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :photos
+  resources :comments, only: [:index, :show, :update, :create, :destroy]
+  resources :photos,only: [:index, :show, :update, :create, :destroy]
+  patch "/photos/:id/like", to: "photos#increment_likes"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
