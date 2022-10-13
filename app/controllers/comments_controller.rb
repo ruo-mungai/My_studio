e99ee81c5b0817d58830bd6524dfdb7a08a67c35
Patrick <ruomungai@gmail.com>
2022-10-13 17:09:23 +0300

    setting models and controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,51 @@
 class CommentsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_error
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def index
+       render json: Comment.all,status: :ok
+    end 
+
+   
+
+    def show
+        comment= find_comment
+        render json: comment, status: :ok
+    end
+
+     def update
+        comment = find_comment
+        Comment.update!(comment_params)
+        render json: comment
+    end
+
+    def create
+        comment= Comment.create!(comment_params)
+        render json: comment, status: :created
+    end
+
+
+    def destroy
+        comment = find_comment
+        Comment.destroy
+        head :no_content
+    end
+
+    private
+
+    def render_error
+        render json: { error: "Comment not found" }, status: :not_found
+     end
+    
+    def find_comment
+         Comment.find(params[:id])
+    end
+    
+    def comment_params
+        params.permit(:description, :photo_id)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,6 @@
 class Photo < ApplicationRecord
+    has_many :comments
+    validates :name, presence: true
+    validates :url, presence: true
+    validates :category, inclusion: ["nature","animal","car","building","human","forest"]
 end
