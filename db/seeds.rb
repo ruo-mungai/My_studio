cad4855a92629fef59f131c7f1427c99ff95085a
Patrick <ruomungai@gmail.com>
2022-10-13 17:30:47 +0300

    creating migration and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+#  Photo.create!( name: "Karura forest", url: "https://www.pexels.com/photo/black-hanging-bridge-surrounded-by-green-forest-trees-775201/" ,category: "forest")
+
+  Comment.create!(description: "great", photo_id: 1)
