cf0c61be434825d97dea3ea687443c942d6d61ae
Patrick <ruomungai@gmail.com>
2022-10-13 20:43:42 +0300

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Home from "./components/Home";
+import Service from "./components/Service";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Photos from "./components/Photos";
+import AddPhoto from "./components/AddPhoto";
+// import Login from "./components/Login"
 
 function App() {
+  const [photo, setPhoto] = useState([]);
+  const [comments, setComments] = useState([]);
+
+  function updateComments(id, comments) {
+    const myComments = photo.map((pic) => {
+      if (pic.id === id) {
+        return { ...photo, comments };
+      } else {
+        return photo;
+      }
+    });
+    setPhoto(myComments);
+  }
+
+  useEffect(() => {
+    fetch("/photos")
+      .then((res) => res.json())
+      .then((data) => {
+        setPhoto(data);
+      });
+  }, []);
+
+  console.log(comments);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Login/> */}
+      <Navbar />
+      <Home />
+      <Service />
+      <Footer />
+      <Photos photo={photo} setPhoto={setPhoto} setComments={setComments} comments={comments} />
+      <AddPhoto setPhoto={setPhoto} photo={photo} updateComments={updateComments} />
     </div>
   );
 }
